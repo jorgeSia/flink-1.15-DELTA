4e96e724adab050ca327cf5f988bfbcae52ab0c6
jorgeSia <georgedrfsia@gmail.com>
2022-03-22 17:13:07 +0100

    Update KeyGroupRangeAssignment to match the 1.12-DELTA version.

--- flink-runtime/src/main/java/org/apache/flink/runtime/state/KeyGroupRangeAssignment.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/state/KeyGroupRangeAssignment.java
@@ -19,6 +19,8 @@
 package org.apache.flink.runtime.state;
 
 import org.apache.flink.api.dag.Transformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.util.MathUtils;
 import org.apache.flink.util.Preconditions;
 
@@ -62,7 +64,20 @@ public final class KeyGroupRangeAssignment {
      */
     public static int assignToKeyGroup(Object key, int maxParallelism) {
         Preconditions.checkNotNull(key, "Assigned key must not be null!");
-        return computeKeyGroupForKeyHash(key.hashCode(), maxParallelism);
+        if (key instanceof Integer ){
+            return computeKeyGroupForKeyHash((int) key, maxParallelism);
+        }
+        else if(key instanceof Tuple3){
+            if(((Tuple3<?, ?, ?>) key).f1 instanceof Integer && ((Tuple3<?, ?, ?>) key).f0 instanceof Integer && ((Tuple3<?, ?, ?>) key).f2 instanceof Integer){
+                return computeKeyGroupForKeyHash((int) ((Tuple3<?, ?, ?>) key).f1, maxParallelism);
+            }
+            else{
+                return computeKeyGroupForKeyHash(key.hashCode() & 0x7FFFFFFF, maxParallelism);
+            }
+        }
+        else{
+            return computeKeyGroupForKeyHash(key.hashCode() & 0x7FFFFFFF, maxParallelism);
+        }
     }
 
     /**
